b93723cd886cb8f4ca63c4708a56b65feb3ca0dc
rebz135 <rebz135@gmail.com>
2018-08-14 19:53:23 -0700

    Complete basic requirements

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -4,7 +4,7 @@ var VideoList = (props) => (
       <VideoListEntry video={video} changeState={props.changeState}/>
     )}
   </div>
-)
+);
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
